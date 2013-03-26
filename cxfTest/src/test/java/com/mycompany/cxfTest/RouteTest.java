ecab0651c0657344f00acc8e8c15432c7065d9bc
Milan Milas <milanmilas@gmail.com>
2013-03-26 11:59:27 +0000

    working version
    address has been changed to http -> jetty is used instead of CXF

--- cxfTest/src/test/java/com/mycompany/cxfTest/RouteTest.java
+++ cxfTest/src/test/java/com/mycompany/cxfTest/RouteTest.java
@@ -1,9 +1,12 @@
 package com.mycompany.cxfTest;
 
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 
 import org.junit.Ignore;
 import org.junit.Test;
+
+import camelinaction.order.OrderEndpoint;
 
 
 public class RouteTest extends CamelBlueprintTestSupport {
@@ -12,15 +15,28 @@ public class RouteTest extends CamelBlueprintTestSupport {
     protected String getBlueprintDescriptor() {
         return "/OSGI-INF/blueprint/blueprint.xml";
     }
+    
+    // should be the same address as we have in our route
+    private static final String URL = "http://localhost:8678/CxfMtomRouterPayloadModeTest/jaxws-mtom/hello";
+   
+    protected static OrderEndpoint createCXFClient() {
+        // we use CXF to create a client for us as its easier than JAXWS and works
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setServiceClass(OrderEndpoint.class);
+        factory.setAddress(URL);
+        return (OrderEndpoint) factory.create();
+    }
 
     @Test
     public void testRoute() throws Exception {
-        // the route is timer based, so every 5th second a message is send
-        // we should then expect at least one message
-/*        getMockEndpoint("mock:result").expectedMinimumMessageCount(1);
 
-        // assert expectations
-        assertMockEndpointsSatisfied();*/
+        // create the webservice client and send the request
+        OrderEndpoint client = createCXFClient();
+        String out = client.order("car", 1, "milan");
+
+        // assert we got a OK back
+        assertEquals("OK", out);
+        
     	assertEquals(1, 1);
     }
 
